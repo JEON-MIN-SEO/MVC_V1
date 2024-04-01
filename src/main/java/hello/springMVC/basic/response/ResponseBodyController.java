1d4f11196a3ffc737641ca0557cdd49b4dcb9bf8
JEON-MIN-SEO <jmsparrowk@naver.com>
2024-04-01 09:57:50 +0900

    4/1

---
+++ src/main/java/hello/springMVC/basic/response/ResponseBodyController.java
@@ -0,0 +1,31 @@
+package hello.springMVC.basic.response;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException{
+        response.getWriter().write("ok");
+    }
+
+
+    /**
+    * HttpEntity, ResponseEntity(Http Status 추가) * @return
+    */
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+
+}
